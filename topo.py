55033170e3c27daaf24b043383e201771c87fd0a
clement10601 <clement10601@gmail.com>
2015-03-23 16:16:54 +0800

    se

--- topo.py
+++ topo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.node import RemoteController
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
@@ -20,29 +21,37 @@ class FatTreeTopo(Topo):
     """
     def __init__(self,coreSwitch=4,pods=4,agpp=2,egpp=2,hpe=2,**opts):
         Topo.__init__(self, **opts)
+        """define Links types"""
+        linkopts100M = dict(bw=100, delay='0ms', loss=0)
+        linkopts1G = dict(bw=1000, delay='0ms', loss=5)
+
         info('*** Adding Core Switches')
         for cs in range(coreSwitch):
-            switch = self.addSwitch('cs%s'%(cs),
+            sw = self.addSwitch('cs%s'%(cs),
                                     controller=c0,
                                     protocols='OpenFlow13')
         info('*** Adding Pod Switches')
         for pod in range(pods):
             for ag in range(agpp):
-                switch = self.addSwitch('p%sa%s'%(pod, ag),
+                sw = self.addSwitch('p%sa%s'%(pod, ag),
                                         controller=c0,
                                         protocols='OpenFlow13')
             for eg in range(egpp):
-                switch = self.addSwitch('p%se%s'%(pod, eg),
+                sw = self.addSwitch('p%se%s'%(pod, eg),
                                         controller=c0,
                                         protocols='OpenFlow13')
                 for h in range(hpe):
                     host = self.addHost('p%se%sh%s' %(pod, eg, h))
                     link = self.addLink('p%se%s' %(pod, eg),
-                                        'p%se%sh%s' %(pod, eg, h))
+                                        'p%se%sh%s' %(pod, eg, h),
+                                        **linkopts100M)
+    def LinkCoreAg():
+        print("Hey")
+
 
 def simTest():
     topo = FatTreeTopo()
-    net = Mininet(topo=topo, build=False)
+    net = Mininet(topo=topo,link=TCLink,controller=None,build=False)
     ryu_ctl = net.addController('c0',
                                 controller=RemoteController,
                                 ip='127.0.0.1', port=6633)
